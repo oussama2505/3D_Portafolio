c7accf1312a88cf7bbcdb9b8a49f39a93bf78e76
oussama2505 <oussama.brahmi0608@gmail.com>
2024-06-29 20:03:34 +0200

    fix Hemisphere light of Computer Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,36 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <Navbar />
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+        </div>
         <Routes>
           <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/tech" element={<Tech />} />
-          <Route path="/works" element={<Works />} />
-          <Route path="/feedbacks" element={<Feedbacks />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<MainLayout section="about" />} />
+          <Route path="/experience" element={<MainLayout section="experience" />} />
+          <Route path="/tech" element={<MainLayout section="tech" />} />
+          <Route path="/works" element={<MainLayout section="works" />} />
+          <Route path="/feedbacks" element={<MainLayout section="feedbacks" />} />
+          <Route path="/contact" element={<MainLayout section="contact" />} />
         </Routes>
         <div className='relative z-0'>
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
+  );
+}
+
+const MainLayout = ({ section }) => {
+  return (
+    <div>
+      {section === "about" && <About />}
+      {section === "experience" && <Experience />}
+      {section === "tech" && <Tech />}
+      {section === "works" && <Works />}
+      {section === "feedbacks" && <Feedbacks />}
+      {section === "contact" && <Contact />}
+    </div>
   );
 }
 
